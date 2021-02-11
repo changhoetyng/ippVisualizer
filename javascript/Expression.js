44056abbbb7bd5b4462703eb36598d191d7522b1
changhoetyng <hoetyng@gmail.com>
2021-02-11 22:26:47 +0800

    added expression check

--- javascript/Expression.js
+++ javascript/Expression.js
@@ -2,16 +2,25 @@ export function retrieveFunction() {
     var visualizeBtn = document.getElementById("visualizeBtn")
     
     visualizeBtn.onclick = function () {
-        console.log(document.getElementById("dropdownMenuButton").innerHTML)
       const expressionPlaceholder = document.getElementById("expressionPlaceholder").value
-      checkExpressionValid(expressionPlaceholder)  
+      if(checkExpressionValid(expressionPlaceholder)){
+          console.log("conversion began")
+      }
     }
 }
 
 function checkExpressionValid(expressionPlaceholder) {
     var expression = expressionPlaceholder;
+    var dropdownMenuButton = document.getElementById("dropdownMenuButtonType")
+    var visualizeBtn = document.getElementById("visualizeBtn")
     var noOperators = 0;
     var noOperands = 0;
+    var openBrackets = 0;
+    var closeBrackets = 0;
+
+    visualizeBtn.innerHTML = "Visualize"
+    visualizeBtn.classList.add("btn-outline-secondary");
+    visualizeBtn.classList.remove("btn-outline-warning");
 
     for (var i = 0; i < expression.length; i++) {
         var expressionChar = expression.charAt(i);
@@ -23,5 +32,34 @@ function checkExpressionValid(expressionPlaceholder) {
         if(expressionChar.match(/[+|-|*|\/]/i)){
             noOperators++;
         }
+
+        if(expressionChar.match(/[(]/i)){
+            openBrackets++;
+        }
+
+        if(expressionChar.match(/[)]/i)){
+            closeBrackets++;
+        }
     }
+
+    if(noOperands - noOperators !== 1 || closeBrackets != [openBrackets]){
+        visualizeBtn.innerHTML = "Invalid Equation"
+        visualizeBtn.classList.remove("btn-outline-secondary");
+        visualizeBtn.classList.add("btn-outline-warning");
+        return false
+    }
+
+    if(dropdownMenuButton.classList.contains("infixType")){
+        console.log("infix")
+    }
+
+    if(dropdownMenuButton.classList.contains("prefixType")){
+        console.log("prefix")
+    }
+
+    if(dropdownMenuButton.classList.contains("postfixType")){
+        console.log("postfix")
+    }
+
+    return true
 }
--- javascript/Menu.js
+++ javascript/Menu.js
@@ -9,32 +9,75 @@ function closeModal() {
     }
 }
 
-function selector() {
-    var infix = document.getElementById("infixSelect")
-    var prefix = document.getElementById("prefixSelect")
-    var postfix = document.getElementById("postfixSelect")
-    var dropdownMenuButton = document.getElementById("dropdownMenuButton")
+function selectorType() {
+    var infix = document.getElementById("infixSelectType")
+    var prefix = document.getElementById("prefixSelectType")
+    var postfix = document.getElementById("postfixSelectType")
+    var dropdownMenuButton = document.getElementById("dropdownMenuButtonType")
     
     infix.onclick = function () {
-        dropdownMenuButton.innerHTML ="Infix Expression";
+        dropdownMenuButton.innerHTML ="Convert Infix Expression";
         dropdownMenuButton.classList.remove("btn-secondary");
         dropdownMenuButton.classList.add("btn-success");
+        dropdownMenuButton.classList.add("infixType");
+        dropdownMenuButton.classList.remove("prefixType");
+        dropdownMenuButton.classList.remove("postfixType");
     }
 
     prefix.onclick = function () {
-        dropdownMenuButton.innerHTML ="Prefix Expression";
+        dropdownMenuButton.innerHTML ="Convert Prefix Expression";
         dropdownMenuButton.classList.remove("btn-secondary");
         dropdownMenuButton.classList.add("btn-success");
+        dropdownMenuButton.classList.add("prefixType");
+        dropdownMenuButton.classList.remove("infixType");
+        dropdownMenuButton.classList.remove("postfixType");
     }
 
     postfix.onclick = function () {
-        dropdownMenuButton.innerHTML ="Postfix Expression";
+        dropdownMenuButton.innerHTML ="Convert Postfix Expression";
         dropdownMenuButton.classList.remove("btn-secondary");
         dropdownMenuButton.classList.add("btn-success");
+        dropdownMenuButton.classList.add("postfixType");
+        dropdownMenuButton.classList.remove("prefixType");
+        dropdownMenuButton.classList.remove("infixType");
+    }
+}
+
+function selectorConvert() {
+    var infix = document.getElementById("infixSelectConvert")
+    var prefix = document.getElementById("prefixSelectConvert")
+    var postfix = document.getElementById("postfixSelectConvert")
+    var dropdownMenuButton = document.getElementById("dropdownMenuButtonConvert")
+    infix.onclick = function () {
+        dropdownMenuButton.innerHTML ="To Infix Expression";
+        dropdownMenuButton.classList.remove("btn-secondary");
+        dropdownMenuButton.classList.add("btn-success");
+        dropdownMenuButton.classList.add("infixConvert");
+        dropdownMenuButton.classList.add("prefixConvert");
+        dropdownMenuButton.classList.add("postfixConvert");
+    }
+
+    prefix.onclick = function () {
+        dropdownMenuButton.innerHTML ="To Prefix Expression";
+        dropdownMenuButton.classList.remove("btn-secondary");
+        dropdownMenuButton.classList.add("btn-success");
+        dropdownMenuButton.classList.add("prefixConvert");
+        dropdownMenuButton.classList.add("infixConvert");
+        dropdownMenuButton.classList.add("postfixConvert");
+    }
+
+    postfix.onclick = function () {
+        dropdownMenuButton.innerHTML ="To Postfix Expression";
+        dropdownMenuButton.classList.remove("btn-secondary");
+        dropdownMenuButton.classList.add("btn-success");
+        dropdownMenuButton.classList.add("postfixConvert");
+        dropdownMenuButton.classList.add("infixConvert");
+        dropdownMenuButton.classList.add("prefixConvert");
     }
 }
 
 export function menu(){
     closeModal()
-    selector()
+    selectorType()
+    selectorConvert()
 }
