fa79a2f3f815892b58ad7bf870d2b47090fdb38f
changhoetyng <hoetyng@gmail.com>
2021-08-19 02:27:02 +0800

    infix algo done

--- javascript/conversion/Animation/infixToPostfix.js
+++ javascript/conversion/Animation/infixToPostfix.js
@@ -15,12 +15,12 @@ export default async function infixToPostfixStack() {
   async function outStackAnimation() {
     pop();
     let outStack = document
-    .getElementById("outputDisp")
-    .getElementsByClassName("dispExpression");
+      .getElementById("outputDisp")
+      .getElementsByClassName("dispExpression");
 
     let stack = document
-    .getElementById("stackPos")
-    .getElementsByClassName("dispExpression");
+      .getElementById("stackPos")
+      .getElementsByClassName("dispExpression");
 
     let inStack = stack[0];
 
@@ -31,19 +31,25 @@ export default async function infixToPostfixStack() {
         { transform: "translateX(0px)", offset: 0.0 },
         {
           transform:
-            "translateY(" +
-            -yValueInputToStack +
-            "px) translateX(" +
-            0 +
-            "px)",
+            "translateY(" + -yValueInputToStack + "px) translateX(" + 0 + "px)",
           offset: 0.3,
         },
         {
-          transform: "translateY(" + -yValueInputToStack + "px) translateX(" + (outStack.length * 40) + "px)",
+          transform:
+            "translateY(" +
+            -yValueInputToStack +
+            "px) translateX(" +
+            outStack.length * 40 +
+            "px)",
           offset: 0.7,
         },
         {
-          transform: "translateY(" + (((stack.length - 1) * 40) + 74) + "px) translateX(" + (outStack.length * 40) + "px)",
+          transform:
+            "translateY(" +
+            ((stack.length - 1) * 40 + 74) +
+            "px) translateX(" +
+            outStack.length * 40 +
+            "px)",
           offset: 1.0,
         },
       ],
@@ -52,15 +58,34 @@ export default async function infixToPostfixStack() {
         fill: "forwards",
       }
     );
-    xValueInputToStack -= 40;
     yValueInputToStack += 40;
+    xValueInputToOutput2 += 40;
     await timer(duration);
     inStack.style.color = "black";
     inStack.style.backgroundColor = "var(--main-color)";
     $("#outputDisp").append(
       `<div class='dispExpression'>${inStack.innerHTML}</div>`
     );
-    inStack.remove()
+    inStack.remove();
+  }
+
+  async function openBracket() {
+    // Close Brackets
+    xValueInputToStack -= 40;
+    // yValueInputToStack -= 40;
+    // xValueInputToOutput2 -= 40;
+    $("#bottomPanelIn").after(`<div class='dispExpressionEmpty'></div>`);
+
+    // Open Brackets
+    pop();
+    let stack = document
+      .getElementById("stackPos")
+      .getElementsByClassName("dispExpression");
+
+    let inStack = stack[0];
+    // yValueInputToStack += 40;
+    // xValueInputToOutput2 += 40;
+    inStack.remove();
   }
 
   async function inStackAnimation(input) {
@@ -91,10 +116,11 @@ export default async function infixToPostfixStack() {
     );
     xValueInputToStack -= 40;
     yValueInputToStack -= 40;
+    xValueInputToOutput2 -= 40;
     await timer(duration);
     input.style.color = "black";
     input.style.backgroundColor = "var(--main-color)";
-    $("#stackPos").append(
+    $("#stackPos").prepend(
       `<div class='dispExpression'>${input.innerHTML}</div>`
     );
     $("#bottomPanelIn").after(`<div class='dispExpressionEmpty'></div>`);
@@ -194,31 +220,49 @@ export default async function infixToPostfixStack() {
       $("#bottomPanelIn").after(`<div class='dispExpressionEmpty'></div>`);
       input.remove();
       indexStack--;
+      continue;
     }
 
     if (input.innerHTML.match(/[+|\-|*|^|\/]/i)) {
-      xValueInputToOutput2 -= 40;
+      let inputPrec = prec(input.innerHTML);
+      if (isEmpty()) {
+        await inStackAnimation(input);
+        input.remove();
+        indexStack--;
+      } else {
+        if (inputPrec > prec(peek())) {
+          await inStackAnimation(input);
+          input.remove();
+          indexStack--;
+        } else {
+          while (inputPrec <= prec(peek()) && !isEmpty()) {
+            await outStackAnimation();
+          }
+          await inStackAnimation(input);
+          input.remove();
+          indexStack--;
+        }
+      }
+      continue;
+    }
 
+    if (input.innerHTML.match(/[\(]/i)) {
+      yValueInputToOutput = 0;
+      xValueInputToOutput = 0;
       await inStackAnimation(input);
-
       input.remove();
       indexStack--;
-      //   if (isEmpty()) {
-      //   } else {
-      //     if (inputPrec > peekPrec) {
-      //     } else {
-      //       while (inputPrec <= peekPrec && !isEmpty()) {
+      continue;
+    }
 
-      //       }
-      //     }
-      //   }
-      // }
-
-      // if (input.innerHTML.match(/[\(]/i)) {
-      // }
-
-      // if (input.innerHTML.match(/[\)]/i)) {
-      // }
+    if (input.innerHTML.match(/[\)]/i)) {
+      while (!isEmpty() && peek() !== "(") {
+        await outStackAnimation();
+      }
+      await openBracket();
+      input.remove();
+      indexStack--;
+      continue;
     }
   }
   while (!isEmpty()) {
