c55ef849c950878f277c3ad86bde80d15088e226
changhoetyng <hoetyng@gmail.com>
2021-08-20 02:33:20 +0800

    project finalized

--- javascript/conversion/Animation/infixToPostfix.js
+++ javascript/conversion/Animation/infixToPostfix.js
@@ -80,9 +80,9 @@ export default async function infixToPostfixStack() {
       .getElementsByClassName("dispExpression");
 
     let inStack = stack[0];
-    inStack.classList.add("scale-out-left");
-    input.classList.add("scale-out-left");
-    await timer(1000)
+    inStack.classList.add("scale-down-bl");
+    input.classList.add("scale-down-bl");
+    await timer(400);
     $("#bottomPanelIn").after(`<div class='dispExpressionEmpty'></div>`);
     inStack.remove();
   }
@@ -154,10 +154,21 @@ export default async function infixToPostfixStack() {
     .getElementById("inputDisp")
     .getElementsByClassName("dispExpression");
 
+  $("#sideout").animate({ scrollTop: $("#lineOne").height() - 80 }, 250);
+  $("#lineOne").addClass("conditionalActive");
+  await timer(250);
+
   for (var indexStack = 0; indexStack < inputStack.length; indexStack++) {
     let input = inputStack[indexStack];
 
     if (input.innerHTML.match(/[A-Z]/i)) {
+      await $("#sideout").animate(
+        { scrollTop: $("#lineTwentyOne").height() + 200 },
+        500
+      );
+      $("#lineTwentyOne").addClass("conditionalActive");
+      $("#lineTwentyTwo").addClass("pseudocodeActive");
+      await timer(500);
       firstOps = false;
       input.style.color = "black";
       input.style.backgroundColor = "var(--sub-color)";
@@ -219,52 +230,110 @@ export default async function infixToPostfixStack() {
       $("#bottomPanelIn").after(`<div class='dispExpressionEmpty'></div>`);
       input.remove();
       indexStack--;
+      $("#lineTwentyOne").removeClass("conditionalActive");
+      $("#lineTwentyTwo").removeClass("pseudocodeActive");
       continue;
     }
 
     if (input.innerHTML.match(/[+|\-|*|^|\/]/i)) {
+      $("#sideout").animate({ scrollTop: $("#lineOne").height() - 80 }, 500);
+      $("#lineTwo").addClass("conditionalActive");
+      await timer(500);
       let inputPrec = prec(input.innerHTML);
       if (isEmpty()) {
+        $("#sideout").animate({ scrollTop: $("#lineOne").height() - 80 }, 500);
+        $("#lineThree").addClass("conditionalActive");
+        $("#lineFour").addClass("pseudocodeActive");
         await inStackAnimation(input);
         input.remove();
         indexStack--;
+        $("#lineThree").removeClass("conditionalActive");
+        $("#lineFour").removeClass("pseudocodeActive");
       } else {
+        $("#sideout").animate({ scrollTop: $("#lineOne").height() - 80 }, 500);
+        $("#lineFive").addClass("conditionalActive");
+        await timer(500);
         if (inputPrec > prec(peek())) {
+          $("#lineSix").addClass("conditionalActive");
+          $("#lineSeven").addClass("pseudocodeActive");
           await inStackAnimation(input);
           input.remove();
           indexStack--;
+          $("#lineSix").removeClass("conditionalActive");
+          $("#lineSeven").removeClass("pseudocodeActive");
         } else {
+          $("#lineNine").addClass("conditionalActive");
+          $("#lineTen").addClass("conditionalActive");
+          $("#lineEleven").addClass("pseudocodeActive");
           while (inputPrec <= prec(peek()) && !isEmpty()) {
             await outStackAnimation();
           }
+          $("#lineEleven").removeClass("pseudocodeActive");
+          $("#lineTen").removeClass("conditionalActive");
+
+          $("#lineTwelveA").addClass("pseudocodeActive");
           await inStackAnimation(input);
+          $("#lineTwelveA").removeClass("pseudocodeActive");
+
+          $("#lineNine").removeClass("conditionalActive");
           input.remove();
           indexStack--;
         }
+        $("#lineFive").removeClass("conditionalActive");
       }
+      $("#lineTwo").removeClass("conditionalActive");
       continue;
     }
 
     if (input.innerHTML.match(/[\(]/i)) {
+      await $("#sideout").animate(
+        { scrollTop: $("#lineFourteen").height() + 200 },
+        500
+      );
+      $("#lineFourteen").addClass("conditionalActive");
+      $("#lineFifteen").addClass("pseudocodeActive");
       yValueInputToOutput = 0;
       xValueInputToOutput = 0;
       await inStackAnimation(input);
       input.remove();
       indexStack--;
+      $("#lineFourteen").removeClass("conditionalActive");
+      $("#lineFifteen").removeClass("pseudocodeActive");
       continue;
     }
 
     if (input.innerHTML.match(/[\)]/i)) {
+      await $("#sideout").animate(
+        { scrollTop: $("#lineSeventeen").height() + 200 },
+        500
+      );
+      $("#lineSeventeen").addClass("conditionalActive");
+      $("#lineEighteen").addClass("pseudocodeActive");
+      await timer(500);
       while (!isEmpty() && peek() !== "(") {
         await outStackAnimation();
       }
       await openBracket(input);
       input.remove();
       indexStack--;
+      $("#lineSeventeen").removeClass("conditionalActive");
+      $("#lineEighteen").removeClass("pseudocodeActive");
       continue;
     }
+  }
+  $("#lineOne").removeClass("conditionalActive");
+  if (!isEmpty()) {
+    await $("#sideout").animate(
+      { scrollTop: $("#lineTwentyFive").height() + 500 },
+      500
+    );
+    $("#lineTwentyFive").addClass("conditionalActive");
+    $("#lineTwentySix").addClass("pseudocodeActive");
+    await timer(500);
   }
   while (!isEmpty()) {
     await outStackAnimation();
   }
+  $("#lineTwentyFive").removeClass("conditionalActive");
+  $("#lineTwentySix").removeClass("pseudocodeActive");
 }
